97645631d06e268012a024a62770a40ff37893db
brandtryan <emailme@brandtryan.com>
2025-02-17 06:19:23 -0500

    add scroll snapping

--- src/main.mjs
+++ src/main.mjs
@@ -1,3 +1,14 @@
+// import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
+
+// document.getElementById('parallax').animate(
+//     { transform: ['translateY(0)', 'translateY(100px)']},
+//     { fill: 'both',
+//       timeline: new ScrollTimeline({
+//         source: document.documentElement,
+//       }),
+//       rangeStart: new CSSUnitValue(0, 'px'),
+//       rangeEnd: new CSSUnitValue(200, 'px'),
+//     });
 
 document.addEventListener('DOMContentLoaded', () => {
   //HTML has loaded
