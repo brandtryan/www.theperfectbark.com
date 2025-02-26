974528ef3026f24a8b96b4dbf14b693ffe784fe5
brandtryan <emailme@brandtryan.com>
2025-02-25 20:14:20 -0500

    Slot order in template overrides what order slot is in index.html

--- src/components/movingtext.js
+++ src/components/movingtext.js
@@ -4,6 +4,7 @@
 const template = document.createElement('template');
 template.innerHTML = `
   <div>
+    <slot name="line09">Default Text</slot>
     <slot name="line00"></slot>
     <slot name="line01"></slot>
     <slot name="line02"></slot>
@@ -13,7 +14,6 @@ template.innerHTML = `
     <slot name="line06"></slot>
     <slot name="line07"></slot>
     <slot name="line08"></slot>
-    <slot name="line09">Default Text</slot>
   </div>
 `;
 
@@ -25,12 +25,11 @@ class MovingText extends HTMLElement {
     // div.textContent = 'Persistent div from constructor function';
     // shadowRoot.append(div);
 
-    // how do you get content into the element a different way?
-    // A slot:
-    // in template create slot: <slot name="line00"></slot>
-    // in index.html add an element that includes slot="line00"
     // whatever content is put in index slot replaces any content
     // in the template above
+    // Order of slots is determined by TEMPLATE
+    // slot"line09" in index follows line08 but it is
+    // RENDERED above slot"line00"
     let clone = template.content.cloneNode(true);
     shadowRoot.append(clone);
   }
