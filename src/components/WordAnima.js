d55f7273093022e22b79915ab6c8c5e8a18e56c3
brandtryan <emailme@brandtryan.com>
2025-03-04 08:02:25 -0500

    Add observed attributes and sync most with properties

--- src/components/WordAnima.js
+++ src/components/WordAnima.js
@@ -40,6 +40,9 @@ class WordAnima extends HTMLElement {
   get id() {
     return this.getAttribute('id');
   }
+  get text() {
+    return this.getAttribute('text');
+  }
   get enable() {
     return this.getAttribute('enable');
   }
@@ -56,7 +59,49 @@ class WordAnima extends HTMLElement {
     return this.getAttribute('timing');
   }
   set timing(value) {
-    return JSON.parse(this.setAttribute('timing'));
+    this.setAttribute('timing', value);
+  }
+  get play() {
+    return this.getAttribute('play');
+  }
+  set play(value) {
+    this.setAttribute('play', value)
+  }
+  get pause() {
+    return this.getAttribute('pause');
+  }
+  set pause(value) {
+    this.setAttribute('pause', value)
+  }
+  get seek() {
+    return this.getAttribute('seek');
+  }
+  set seek(value) {
+    this.setAttribute('seek', value)
+  }
+  get reverse() {
+    return this.getAttribute('reverse');
+  }
+  set reverse(value) {
+    this.setAttribute('reverse', value)
+  }
+  get currenttime() {
+    return this.getAttribute('currenttime');
+  }
+  set currenttime(value) {
+    this.setAttribute('currenttime', value)
+  }
+  get playbackrate() {
+    return this.getAttribute('playbackrate');
+  }
+  set playbackrate(value) {
+    this.setAttribute('playbackrate', value)
+  }
+  get play() {
+    return this.getAttribute('play');
+  }
+  set play(value) {
+    this.setAttribute('play', value)
   }
 
   //
@@ -72,41 +117,20 @@ class WordAnima extends HTMLElement {
       // create/get new timeline
       // create new animation(KeyFrameEffect, timeline)
     }
-    if (attrName === 'control' && newVal) {
-      this._handleControl(newVal);
+    if (attrName === 'timing') {
+      // pass to new animation
     }
-    if (attrName === 'seek' && newVal) {
-      this._handleSeek(parseFloat(newVal));
+    if (attrName === 'play') {
+
     }
-    if (attrName === 'delay') {
-      // add to timing object
+    if (attrName === 'pause') {
+
     }
-    if (attrName === 'direction') {
-      // add to timing object
+    if (attrName === 'seek') {
+
     }
-    if (attrName === 'duration') {
-      // add to timing object
-    }
-    if (attrName === 'easing') {
-      // add to timing object
-    }
-    if (attrName === 'enddelay') {
-      // add to timing object
-    }
-    if (attrName === 'iterationstart') {
-      // add to timing object
-    }
-    if (attrName === 'iterations') {
-      // add to timing object
-    }
-    if (attrName === 'playbackrate') {
-      // check animation object complete
-      // animation.playbackrate = 1
-      // animation.play
-    }
-    if (attrName === 'starttime') {
-      // check animation object complete
-      // animation.starttime = 
+    if (attrName === 'reverse') {
+
     }
     if (attrName === 'currenttime') {
       // reset animations and pause them
@@ -114,6 +138,11 @@ class WordAnima extends HTMLElement {
       //  animation.pause();
       //  animation.currenttime=0; // "start page over option?"
       //})
+    }
+    if (attrName === 'playbackrate') {
+      // check animation object complete
+      // animation.playbackrate = 1
+      // animation.play
     }
   }
 
